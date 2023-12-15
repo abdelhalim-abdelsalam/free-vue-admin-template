df6ace48298ed711bc9465f23022a0c23d3a8559
abdelhalim-abdelsalam <108537602+abdelhalim-abdelsalam@users.noreply.github.com>
2023-12-15 15:48:36 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,24 @@
+### Installation
+
+```bash
+$ npm install
+```
+
+or
+
+```bash
+$ yarn install
+```
+
+# dev server with hot reload at http://localhost:3000
+
+$ npm run serve
+
+````
+
+or
+
+``` bash
+# dev server with hot reload at http://localhost:3000
+$ yarn serve
+````
---
+++ src/components/AppFooter.vue
@@ -0,0 +1,19 @@
+<template>www</template>
+
+<script>
+import { reactive, toRefs } from 'vue'
+
+export default {
+  setup() {
+    const state = reactive({
+      count: 0,
+    })
+
+    return {
+      ...toRefs(state),
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
